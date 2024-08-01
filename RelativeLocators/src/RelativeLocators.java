1559971346637d65f9ff05d917df456c47082bdb
nidhisingh25901 <nidhisingh25901@gmail.com>
2024-08-02 01:06:43 +0530

    Seventh Commit

---
+++ RelativeLocators/src/RelativeLocators.java
@@ -0,0 +1,24 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import static org.openqa.selenium.support.locators.RelativeLocator.*;
+public class RelativeLocators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	       System.setProperty("webdriver.chrome.driver",
+	                "E:\\Driver\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();  
+		driver.manage().window().maximize(); 
+		
+		driver.get("https://rahulshettyacademy.com/angularpractice/");  
+		
+		WebElement namEditBox = driver.findElement(By.cssSelector("[name='name']")); 
+		
+		System.out.println(driver.findElement(with(By.tagName("label")).above(namEditBox))); 
+		
+		
+	}
+
+}
